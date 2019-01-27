c0c58739d3846e21c47a8cf9954ea4abbb405cea
albornzineCahill <legojonah39@gmail.com>
2019-01-27 17:13:47 -0600

    Refactor

--- SummonHelper(windows)/SummonHelper(windows)/PresetData/ConjureElementals.cs
+++ SummonHelper(windows)/SummonHelper(windows)/PresetData/ConjureElementals.cs
@@ -19,55 +19,55 @@ namespace SummonHelper_windows_.PresetData
         {
             List<Preset> ret = new List<Preset>();
 
-            ret.Add(new Preset("Azer", getCount(2, minor),5,1,8,3));
-            ret.Add(new Preset("Air Elemental", getCount(5, minor),8,2,8,5));
-            ret.Add(new Preset("Earth Elemental", getCount(5, minor),8,2,8,5));
-            ret.Add(new Preset("Fire Elemental", getCount(5, minor),6,2,6,3));
-            ret.Add(new Preset("Water Elemental", getCount(5, minor),7,2,8,4));
-            ret.Add(new Preset("Galeb Duhr", getCount(6, minor),8,2,6,5));
-            ret.Add(new Preset("Gargoyle(Bite)", getCount(2, minor),4,1,6,2));
-            ret.Add(new Preset("Gargoyle(Claws)", getCount(2, minor), 4, 1, 6, 2));
-            ret.Add(new Preset("Dao(Fist)", getCount(11, minor),10,2,8,6));
-            ret.Add(new Preset("Dao(Maul)", getCount(11, minor), 10, 4, 6, 6));
-            ret.Add(new Preset("Djinni", getCount(11, minor),9,2,6,5));
-            ret.Add(new Preset("Efreeti(scimitar)", getCount(11, minor),10,4,6,6));
-            ret.Add(new Preset("Efreeti(Hurl Flame)", getCount(11, minor), 7, 5, 6, 0));
-            ret.Add(new Preset("Marid(1H)", getCount(11, minor),10,2,6,6));
-            ret.Add(new Preset("Marid(2H)", getCount(11, minor), 10, 2, 8, 6));
-            ret.Add(new Preset("Invisible Stalker", getCount(6, minor),6,2,6,3));
-            ret.Add(new Preset("Magmin", getCount(.5, minor),4,2,6,0));
-            ret.Add(new Preset("Dust Mephit", getCount(.5, minor),3,3,4,1));
-            ret.Add(new Preset("Magma Mephit", getCount(.5, minor),3,2,4,1));
-            ret.Add(new Preset("Ice Mephit", getCount(.5, minor),3,2,4,1));
-            ret.Add(new Preset("Mud Mephit", getCount(.25, minor),3,1,6,1));
-            ret.Add(new Preset("Smoke Mephit", getCount(.25, minor),4,1,4,2));
-            ret.Add(new Preset("Steam Mephit", getCount(.25, minor),2,2,4,0));
-            ret.Add(new Preset("fire Snake(Bite/tail)", getCount(1, minor),3,1,4,1));
-            ret.Add(new Preset("Salamander(Spear 1h)", getCount(5, minor),7,3,6,4));
-            ret.Add(new Preset("Salamander(Spear 2h)", getCount(5, minor), 7, 2, 8, 4));
-            ret.Add(new Preset("Salamander(Tail)", getCount(5, minor), 7, 4, 6, 4));
-            ret.Add(new Preset("Water weird", getCount(3, minor),5,3,6,3));
-            ret.Add(new Preset("Blistercoil Weird(Medium)", getCount(4, minor),5,1,8,3));
-            ret.Add(new Preset("Blistercoil Weird(Large)", getCount(4, minor), 5, 2, 8, 3));
-            ret.Add(new Preset("Fluxcharger(slam)", getCount(7, minor),7,2,8,4));
-            ret.Add(new Preset("Fluxcharger(Arc Lightning)", getCount(7, minor), 7, 3, 10, 0));
-            ret.Add(new Preset("Galvanice Weird", getCount(1, minor),4,1,6,2));
-            ret.Add(new Preset("Frost Salamader(Claw)", getCount(9, minor),9,1,6,5));
-            ret.Add(new Preset("Frost Salamader(Bite)", getCount(9, minor), 9, 1, 8, 5));
-            ret.Add(new Preset("Leviathan(Slam)", getCount(20, minor),16,1,10,10));
-            ret.Add(new Preset("Leviathan(Tail)", getCount(20, minor), 16, 1, 12, 10));
-            ret.Add(new Preset("Phonix(Beak)", getCount(16, minor),13,2,6,8));
-            ret.Add(new Preset("Phonix(Talons)", getCount(16, minor), 13, 2, 8, 8));
-            ret.Add(new Preset("Elder Tempest", getCount(23, minor),16,4,6,9));
-            ret.Add(new Preset("Zaratan(Bite)", getCount(22, minor),17,4,8,10));
-            ret.Add(new Preset("Zaratan(stomp)", getCount(22, minor), 17, 3, 10, 10));
-            ret.Add(new Preset("Zaratan(Spit Rock)", getCount(22, minor), 17, 6, 8, 10));
-            ret.Add(new Preset("Air Elemental Myrmidon", getCount(7, minor),7,1,8,4));
-            ret.Add(new Preset("Earth Elemental Myrmidon", getCount(7, minor), 7,2, 6, 4));
-            ret.Add(new Preset("Fire Elemental Myrmidon", getCount(7, minor), 7, 1, 6, 4));
-            ret.Add(new Preset("Water Elemental Myrmidon(1H)", getCount(7, minor), 7, 1, 6, 4));
-            ret.Add(new Preset("Water Elemental Myrmidon(2H)", getCount(7, minor), 7, 1, 8, 4));
-            ret.Add(new Preset("Flail Snail", getCount(3,minor),5,1,6,3));
+            ret.Add(new Preset("Azer", getCount(2),5,1,8,3));
+            ret.Add(new Preset("Air Elemental", getCount(5),8,2,8,5));
+            ret.Add(new Preset("Earth Elemental", getCount(5),8,2,8,5));
+            ret.Add(new Preset("Fire Elemental", getCount(5),6,2,6,3));
+            ret.Add(new Preset("Water Elemental", getCount(5),7,2,8,4));
+            ret.Add(new Preset("Galeb Duhr", getCount(6),8,2,6,5));
+            ret.Add(new Preset("Gargoyle(Bite)", getCount(2),4,1,6,2));
+            ret.Add(new Preset("Gargoyle(Claws)", getCount(2), 4, 1, 6, 2));
+            ret.Add(new Preset("Dao(Fist)", getCount(11),10,2,8,6));
+            ret.Add(new Preset("Dao(Maul)", getCount(11), 10, 4, 6, 6));
+            ret.Add(new Preset("Djinni", getCount(11),9,2,6,5));
+            ret.Add(new Preset("Efreeti(scimitar)", getCount(11),10,4,6,6));
+            ret.Add(new Preset("Efreeti(Hurl Flame)", getCount(11), 7, 5, 6, 0));
+            ret.Add(new Preset("Marid(1H)", getCount(11),10,2,6,6));
+            ret.Add(new Preset("Marid(2H)", getCount(11), 10, 2, 8, 6));
+            ret.Add(new Preset("Invisible Stalker", getCount(6),6,2,6,3));
+            ret.Add(new Preset("Magmin", getCount(.5),4,2,6,0));
+            ret.Add(new Preset("Dust Mephit", getCount(.5),3,3,4,1));
+            ret.Add(new Preset("Magma Mephit", getCount(.5),3,2,4,1));
+            ret.Add(new Preset("Ice Mephit", getCount(.5),3,2,4,1));
+            ret.Add(new Preset("Mud Mephit", getCount(.25),3,1,6,1));
+            ret.Add(new Preset("Smoke Mephit", getCount(.25),4,1,4,2));
+            ret.Add(new Preset("Steam Mephit", getCount(.25),2,2,4,0));
+            ret.Add(new Preset("fire Snake(Bite/tail)", getCount(1),3,1,4,1));
+            ret.Add(new Preset("Salamander(Spear 1h)", getCount(5),7,3,6,4));
+            ret.Add(new Preset("Salamander(Spear 2h)", getCount(5), 7, 2, 8, 4));
+            ret.Add(new Preset("Salamander(Tail)", getCount(5), 7, 4, 6, 4));
+            ret.Add(new Preset("Water weird", getCount(3),5,3,6,3));
+            ret.Add(new Preset("Blistercoil Weird(Medium)", getCount(4),5,1,8,3));
+            ret.Add(new Preset("Blistercoil Weird(Large)", getCount(4), 5, 2, 8, 3));
+            ret.Add(new Preset("Fluxcharger(slam)", getCount(7),7,2,8,4));
+            ret.Add(new Preset("Fluxcharger(Arc Lightning)", getCount(7), 7, 3, 10, 0));
+            ret.Add(new Preset("Galvanice Weird", getCount(1),4,1,6,2));
+            ret.Add(new Preset("Frost Salamader(Claw)", getCount(9),9,1,6,5));
+            ret.Add(new Preset("Frost Salamader(Bite)", getCount(9), 9, 1, 8, 5));
+            ret.Add(new Preset("Leviathan(Slam)", getCount(20),16,1,10,10));
+            ret.Add(new Preset("Leviathan(Tail)", getCount(20), 16, 1, 12, 10));
+            ret.Add(new Preset("Phonix(Beak)", getCount(16),13,2,6,8));
+            ret.Add(new Preset("Phonix(Talons)", getCount(16), 13, 2, 8, 8));
+            ret.Add(new Preset("Elder Tempest", getCount(23),16,4,6,9));
+            ret.Add(new Preset("Zaratan(Bite)", getCount(22),17,4,8,10));
+            ret.Add(new Preset("Zaratan(stomp)", getCount(22), 17, 3, 10, 10));
+            ret.Add(new Preset("Zaratan(Spit Rock)", getCount(22), 17, 6, 8, 10));
+            ret.Add(new Preset("Air Elemental Myrmidon", getCount(7),7,1,8,4));
+            ret.Add(new Preset("Earth Elemental Myrmidon", getCount(7), 7,2, 6, 4));
+            ret.Add(new Preset("Fire Elemental Myrmidon", getCount(7), 7, 1, 6, 4));
+            ret.Add(new Preset("Water Elemental Myrmidon(1H)", getCount(7), 7, 1, 6, 4));
+            ret.Add(new Preset("Water Elemental Myrmidon(2H)", getCount(7), 7, 1, 8, 4));
+            ret.Add(new Preset("Flail Snail", getCount(3),5,1,6,3));
 
             return ret.ToArray();
         }
@@ -85,7 +85,7 @@ namespace SummonHelper_windows_.PresetData
             return ret.ToArray();
         }
 
-        public int getCount(double cr, bool minor)
+        public int getCount(double cr)
         {
             if(minor == true)
             {
--- SummonHelper(windows)/SummonHelper(windows)/PresetData/ConjureFey.cs
+++ SummonHelper(windows)/SummonHelper(windows)/PresetData/ConjureFey.cs
@@ -20,47 +20,45 @@ namespace SummonHelper_windows_.PresetData
         {
             List<Preset> ret = new List<Preset>();
 
-            ret.Add(new Preset("Dryad", getCount(1, minor),2,1,4,0));
-            ret.Add(new Preset("Green Hag", getCount(3, minor),6,2,8,4));
-            ret.Add(new Preset("Night Hag", getCount(5, minor),7,2,8,4));
-            ret.Add(new Preset("Sea Hag", getCount(2, minor),5,2,6,3));
-            ret.Add(new Preset("Pixie", getCount(.25, minor),0,0,0,0));
-            ret.Add(new Preset("Satyr(Ram)", getCount(.5, minor), 3, 2, 4, 1));
-            ret.Add(new Preset("Satyr(Shortsword)", getCount(.5, minor), 5, 1, 6, 3));
-            ret.Add(new Preset("Satyr(Shortbow)", getCount(.5, minor), 5, 1, 6, 3));
-            ret.Add(new Preset("Sprite(longsword)", getCount(.25, minor),2,1,1,0));
-            ret.Add(new Preset("Sprite(shortbow)", getCount(.25, minor), 6, 1, 1, 0));
-            ret.Add(new Preset("Blink Dog", getCount(.25, minor),3,1,6,1));
-            ret.Add(new Preset("Autumn Eladrin(longsword 1H)", getCount(10, minor),5,1,8,1));
-            ret.Add(new Preset("Autumn Eladrin(longsword 2H)", getCount(10, minor), 5, 1, 10, 1));
-            ret.Add(new Preset("Autumn Eladrin(Longbow)", getCount(10, minor), 7, 1, 8, 3));
-            ret.Add(new Preset("Spring Eladrin(longsword 1H)", getCount(10, minor), 6, 1, 8, 2));
-            ret.Add(new Preset("Spring Eladrin(longsword 2H)", getCount(10, minor), 6, 1, 10, 2));
-            ret.Add(new Preset("Spring Eladrin(Longbow)", getCount(10, minor), 7, 1, 8, 3));
-            ret.Add(new Preset("Summer Eladrin(longsword 1H)", getCount(10, minor), 8, 2, 8, 4));
-            ret.Add(new Preset("Summer Eladrin(longsword 2H)", getCount(10, minor), 8, 2, 10, 4));
-            ret.Add(new Preset("Summer Eladrin(Longbow)", getCount(10, minor), 9, 2, 8, 5));
-            ret.Add(new Preset("Winter Eladrin(longsword 1H)", getCount(10, minor), 4, 1, 8, 0));
-            ret.Add(new Preset("Winter Eladrin(longsword 2H)", getCount(10, minor),4, 1, 10, 0));
-            ret.Add(new Preset("Winter Eladrin(Longbow)", getCount(10, minor), 4, 1, 8, 0));
-            ret.Add(new Preset("Conclave Dryad(vine staff)", getCount(9, minor),9,2,6,5));
-            ret.Add(new Preset("Conclave Dryad(Longbow)", getCount(9, minor), 8, 1, 8, 4));
-            ret.Add(new Preset("Trostani(contrict)", getCount(18, minor),11,3,6,5));
-            ret.Add(new Preset("Trostani(Touch of order)", getCount(18, minor), 16, 3, 8, 10));
-            ret.Add(new Preset("Boggle", 8, 1, 1, 6, -1));
-            ret.Add(new Preset("Darkling", 4, 5, 1, 4, 3));
-            ret.Add(new Preset("Darkling Elder", 1, 5, 1, 6, 3));
-            ret.Add(new Preset("Annis Hag(bite)",getCount(6,minor),8,3,6,5));
-            ret.Add(new Preset("Annis Hag(claw)", getCount(6, minor), 8, 3, 6, 5));
-            ret.Add(new Preset("Bheur Hag",getCount(7,minor),4,2,8,1));
-            ret.Add(new Preset("Korred(great Club)", getCount(7, minor), 9, 1, 8, 6));
-            ret.Add(new Preset("Korred(Rock)", getCount(7, minor), 9, 2, 8, 6));
-            ret.Add(new Preset("Meenlock", getCount(2, minor), 4, 2, 4, 2));
-            ret.Add(new Preset("quickling", getCount(1, minor), 8, 1, 4, 6));
-            ret.Add(new Preset("Redcap", getCount(3, minor), 6, 2, 4, 4));
-            ret.Add(new Preset("Yeth", getCount(4, minor), 6, 2, 6, 4));
-
-
+            ret.Add(new Preset("Dryad", getCount(1),2,1,4,0));
+            ret.Add(new Preset("Green Hag", getCount(3),6,2,8,4));
+            ret.Add(new Preset("Night Hag", getCount(5),7,2,8,4));
+            ret.Add(new Preset("Sea Hag", getCount(2),5,2,6,3));
+            ret.Add(new Preset("Pixie", getCount(.25),0,0,0,0));
+            ret.Add(new Preset("Satyr(Ram)", getCount(.5), 3, 2, 4, 1));
+            ret.Add(new Preset("Satyr(Shortsword)", getCount(.5), 5, 1, 6, 3));
+            ret.Add(new Preset("Satyr(Shortbow)", getCount(.5), 5, 1, 6, 3));
+            ret.Add(new Preset("Sprite(longsword)", getCount(.25),2,1,1,0));
+            ret.Add(new Preset("Sprite(shortbow)", getCount(.25), 6, 1, 1, 0));
+            ret.Add(new Preset("Blink Dog", getCount(.25),3,1,6,1));
+            ret.Add(new Preset("Autumn Eladrin(longsword 1H)", getCount(10),5,1,8,1));
+            ret.Add(new Preset("Autumn Eladrin(longsword 2H)", getCount(10), 5, 1, 10, 1));
+            ret.Add(new Preset("Autumn Eladrin(Longbow)", getCount(10), 7, 1, 8, 3));
+            ret.Add(new Preset("Spring Eladrin(longsword 1H)", getCount(10), 6, 1, 8, 2));
+            ret.Add(new Preset("Spring Eladrin(longsword 2H)", getCount(10), 6, 1, 10, 2));
+            ret.Add(new Preset("Spring Eladrin(Longbow)", getCount(10), 7, 1, 8, 3));
+            ret.Add(new Preset("Summer Eladrin(longsword 1H)", getCount(10), 8, 2, 8, 4));
+            ret.Add(new Preset("Summer Eladrin(longsword 2H)", getCount(10), 8, 2, 10, 4));
+            ret.Add(new Preset("Summer Eladrin(Longbow)", getCount(10), 9, 2, 8, 5));
+            ret.Add(new Preset("Winter Eladrin(longsword 1H)", getCount(10), 4, 1, 8, 0));
+            ret.Add(new Preset("Winter Eladrin(longsword 2H)", getCount(10),4, 1, 10, 0));
+            ret.Add(new Preset("Winter Eladrin(Longbow)", getCount(10), 4, 1, 8, 0));
+            ret.Add(new Preset("Conclave Dryad(vine staff)", getCount(9),9,2,6,5));
+            ret.Add(new Preset("Conclave Dryad(Longbow)", getCount(9), 8, 1, 8, 4));
+            ret.Add(new Preset("Trostani(contrict)", getCount(18),11,3,6,5));
+            ret.Add(new Preset("Trostani(Touch of order)", getCount(18), 16, 3, 8, 10));
+            ret.Add(new Preset("Boggle", getCount((1 / 8)), 1, 1, 6, -1));
+            ret.Add(new Preset("Darkling", getCount(.5), 5, 1, 4, 3));
+            ret.Add(new Preset("Darkling Elder", getCount(2), 5, 1, 6, 3));
+            ret.Add(new Preset("Annis Hag(bite)",getCount(6),8,3,6,5));
+            ret.Add(new Preset("Annis Hag(claw)", getCount(6), 8, 3, 6, 5));
+            ret.Add(new Preset("Bheur Hag",getCount(7),4,2,8,1));
+            ret.Add(new Preset("Korred(great Club)", getCount(7), 9, 1, 8, 6));
+            ret.Add(new Preset("Korred(Rock)", getCount(7), 9, 2, 8, 6));
+            ret.Add(new Preset("Meenlock", getCount(2), 4, 2, 4, 2));
+            ret.Add(new Preset("quickling", getCount(1), 8, 1, 4, 6));
+            ret.Add(new Preset("Redcap", getCount(3), 6, 2, 4, 4));
+            ret.Add(new Preset("Yeth", getCount(4), 6, 2, 6, 4));
 
             return ret.ToArray();
         }
@@ -77,7 +75,7 @@ namespace SummonHelper_windows_.PresetData
 
             return ret.ToArray();
         }
-        public int getCount(double cr, bool minor)
+        public int getCount(double cr)
         {
             if (minor == true)
             {
